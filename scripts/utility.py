b875b56a95b7487df3da7294a9714e18b5e0ece0
flamedash2358 <lucykooistra@icloud.com>
2025-01-09 03:07:28 -0800

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -2518,7 +2518,7 @@ def generate_sprite(
             )
 
         # draw non-boba eyes
-        if cat.pelt.eye_colour not in Pelt.riveye_colours:
+        if cat.pelt.eye_colour not in Pelt.riveye_colours or Pelt.buttoneye_colours:
             eyes = sprites.sprites["eyes" + cat.pelt.eye_colour + cat_sprite].copy()
             if cat.pelt.eye_colour2 != None:
                 eyes.blit(
