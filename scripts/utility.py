c9ceb904b8cf8b752f1b21352aa807353453203d
flamedash2358 <lucykooistra@icloud.com>
2025-01-09 08:56:16 -0800

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -2518,7 +2518,16 @@ def generate_sprite(
             )
 
         # draw non-boba eyes
-        if cat.pelt.eye_colour not in Pelt.riveye_colours:
+        if cat.pelt.eye_colour not in Pelt.riveye_colours and not in Pelt.buttoneye_colours:
+            eyes = sprites.sprites["eyes" + cat.pelt.eye_colour + cat_sprite].copy()
+            if cat.pelt.eye_colour2 != None:
+                eyes.blit(
+                    sprites.sprites["eyes2" + cat.pelt.eye_colour2 + cat_sprite], (0, 0)
+                )
+            new_sprite.blit(eyes, (0, 0))
+            
+        # draw riv boba eyes
+        if cat.pelt.eye_colour in Pelt.riveye_colours and not in Pelt.buttoneye_colours:
             eyes = sprites.sprites["eyes" + cat.pelt.eye_colour + cat_sprite].copy()
             if cat.pelt.eye_colour2 != None:
                 eyes.blit(
@@ -2526,6 +2535,15 @@ def generate_sprite(
                 )
             new_sprite.blit(eyes, (0, 0))
 
+        # draw button boba eyes
+        if cat.pelt.eye_colour in Pelt.buttoneye_colours:
+            eyes = sprites.sprites["eyes" + cat.pelt.eye_colour + cat_sprite].copy()
+            if cat.pelt.eye_colour2 != None:
+                eyes.blit(
+                    sprites.sprites["eyes2" + cat.pelt.eye_colour2 + cat_sprite], (0, 0)
+                )
+            new_sprite.blit(eyes, (0, 0))
+            
         # draw scars1
         if not scars_hidden:
             for scar in cat.pelt.scars:
@@ -2553,16 +2571,7 @@ def generate_sprite(
             new_sprite.blit(sprites.sprites["lineartdf" + cat_sprite], (0, 0))
         elif dead:
             new_sprite.blit(sprites.sprites["lineartdead" + cat_sprite], (0, 0))
-            
-        # draw riv boba eyes
-        if cat.pelt.eye_colour in Pelt.riveye_colours:
-            eyes = sprites.sprites["eyes" + cat.pelt.eye_colour + cat_sprite].copy()
-            if cat.pelt.eye_colour2 != None:
-                eyes.blit(
-                    sprites.sprites["eyes2" + cat.pelt.eye_colour2 + cat_sprite], (0, 0)
-                )
-            new_sprite.blit(eyes, (0, 0))
-            
+        
         # draw skin and scars2
         blendmode = pygame.BLEND_RGBA_MIN
         new_sprite.blit(sprites.sprites["skin" + cat.pelt.skin + cat_sprite], (0, 0))
